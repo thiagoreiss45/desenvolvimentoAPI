7054f5412be759c0d549438aaf59713c7fe4d02c
thaigo <thg@DESKTOP-S5BCLQS.localdomain>
2020-11-16 10:32:28 -0300

    alterando testes

--- part-1/teste/testeApi.sh
+++ part-1/teste/testeApi.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-for counter in $(seq 0 599)
+for counter in $(seq 0 570)
 do
 	if ! (($counter % 10));	then
 		curl -XPOST http://127.0.0.1:5000/linx.com/products/ -d '[{"id": 0, "name":"mesa"}]'
-
+		
 	fi
-	sleep 1
+	
   curl -X POST http://127.0.0.1:5000/linx.com/products/ -d '[{"id": '"$counter"', "name":"mesa"}]'
+  sleep 1
 
 done
